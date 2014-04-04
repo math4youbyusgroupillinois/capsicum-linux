05a1fdc58d0b721096a5b065d0e7e2bacccde1a3
David Drysdale <drysdale@google.com>
2014-04-04 16:41:56 +0100

    syscalls,x86: implement execveat() system call
    This patch set adds execveat(2) for x86, and is derived from Meredydd
    Luff's patch from Sept 2012 (https://lkml.org/lkml/2012/9/11/528).
    
    The primary aim of adding an execveat syscall is to allow an
    implementation of fexecve(3) that does not rely on the /proc
    filesystem.  The current glibc version of fexecve(3) is implemented
    via /proc, which causes problems in sandboxed or otherwise restricted
    environments.
    
    Given the desire for a /proc-free fexecve() implementation, HPA
    suggested (https://lkml.org/lkml/2006/7/11/556) that an execveat(2)
    syscall would be an appropriate generalization.
    
    Also, having a new syscall means that it can take a flags argument
    without back-compatibility concerns.  The current implementation just
    defines the AT_SYMLINK_NOFOLLOW flag, but other flags could be added
    in future -- for example, flags for new namespaces (as suggested at
    https://lkml.org/lkml/2006/7/11/474).
    
    Related history:
     - https://lkml.org/lkml/2006/12/27/123 is an example of someone
       realizing that fexecve() is likely to fail in a chroot environment.
     - http://bugs.debian.org/cgi-bin/bugreport.cgi?bug=514043 covered
       documenting the /proc requirement of fexecve(3) in its manpage, to
       "prevent other people from wasting their time".
     - https://bugzilla.kernel.org/show_bug.cgi?id=74481 documented that
       it's not possible to fexecve() a file descriptor for a script with
       close-on-exec set (which is possible with the implementation here).
     - https://bugzilla.redhat.com/show_bug.cgi?id=241609 described a
       problem where a process that did setuid() could not fexecve()
       because it no longer had access to /proc/self/fd; this has since
       been fixed.
    
    This patch in particular (of 3 = 2 kernel + 1 man-pages):
    
    Adds the new system execveat(2) syscall. execveat() is to execve() as
    openat() is to open(): it takes a file descriptor that refers to a
    directory, and resolves the filename relative to that.
    
    In addition, if the filename is NULL, execveat() executes the file
    to which the file descriptor refers. This replicates the functionality
    of fexecve(), which is a system call in other UNIXen, but in Linux
    glibc it depends on /proc being mounted.
    
    Only x86-64, i386 and x32 ABIs are supported in this patch.
    
    Based on patches by Meredydd Luff <meredydd@senatehouse.org>
    
    Signed-off-by: David Drysdale <drysdale@google.com>

--- include/uapi/asm-generic/unistd.h
+++ include/uapi/asm-generic/unistd.h
@@ -699,9 +699,11 @@ __SYSCALL(__NR_sched_setattr, sys_sched_setattr)
 __SYSCALL(__NR_sched_getattr, sys_sched_getattr)
 #define __NR_renameat2 276
 __SYSCALL(__NR_renameat2, sys_renameat2)
+#define __NR_execveat 278
+__SC_COMP(__NR_execveat, sys_execveat, compat_sys_execveat)
 
 #undef __NR_syscalls
-#define __NR_syscalls 277
+#define __NR_syscalls 279
 
 /*
  * All syscalls below here should go away really,
